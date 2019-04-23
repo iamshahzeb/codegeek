96974d614392d5e83308119e3f0eaa74e65f100b
Shahzeb Akhtar <syedshahzebakhtar@gmail.com>
2019-04-23 18:05:53 +0500

    my initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var budgetcontroller = (function(){
+
+})();
+
+var uicontroller = (function(){
+
+})();
+
+
+var controller=(function(budctrl,uictrl){
+
+document.querySelector('.button').addEventListener('click',function(){
+
+});
+
+})(budgetcontroller,uicontroller);
